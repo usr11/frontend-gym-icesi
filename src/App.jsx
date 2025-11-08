b1f0a91bbd61428bbb9982454263b3fbec699259
Juan Pablo Zambrano <juanpza11@gmail.com>
2025-11-07 21:35:12 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="bg-background w-100 h-100"></div>
+      <h1 className="text-text">HOLA</h1>
+    </>
+  );
+}
+
+export default App;
