e4ef1a26c11c125bbd756ec205060549e2b86b3c
Juan Pablo Zambrano <juanpza11@gmail.com>
2025-11-13 05:15:36 -0500

    progress

--- src/api/progress/GetProgressByUser.js
+++ src/api/progress/GetProgressByUser.js
@@ -1,96 +1,97 @@
-const GetProgressByUser = ({userId}) => {
-  return records
-}
-
-export default GetProgressByUser;
-
-
-  const records = [
-    {
-      id: 1,
-      date: "2025-11-01",
-      exercise: "Press de banca",
-      reps: 12,
-      sets: 4,
-      weight: 65,
-      effortLevel: 7,
-    },
-    {
-      id: 2,
-      date: "2025-10-28",
-      exercise: "Sentadillas",
-      reps: 10,
-      sets: 3,
-      weight: 80,
-      effortLevel: 8,
-    },
-    {
-      id: 3,
-      date: "2025-10-25",
-      exercise: "Trote",
-      time: "25 min",
-      distance: "4 km",
-      rhythm: 90,
-      effortLevel: 6,
-    },
-    {
-      id: 4,
-      date: "2025-10-20",
-      exercise: "Peso muerto",
-      reps: 8,
-      sets: 3,
-      weight: 90,
-      effortLevel: 7,
-    },
-    {
-      id: 5,
-      date: "2025-10-15",
-      exercise: "Press militar",
-      reps: 10,
-      sets: 3,
-      weight: 40,
-      effortLevel: 6,
-    },
-    {
-      id: 6,
-      date: "2025-10-10",
-      exercise: "Trote",
-      time: "20 min",
-      distance: "3.5 km",
-      effortLevel: 5,
-    },
-  ];
-
-
-// import { baseurl } from "../../utils/constatn";
-
-// const GetProgressByUser = async ({ userId }) => {
-//   try {
-//     const token = localStorage.getItem("accessToken");
-
-//     if (!token) {
-//       throw new Error("Token no encontrado. Por favor inicia sesión nuevamente.");
-//     }
-
-//     const response = await fetch(`${baseurl}/api/progress/user/${userId}`, {
-//       method: "GET",
-//       headers: {
-//         "Authorization": `Bearer ${token}`,
-//         "Content-Type": "application/json",
-//       },
-//     });
-
-//     if (!response.ok) {
-//       const errorData = await response.json().catch(() => ({}));
-//       throw new Error(errorData.message || "No se pudieron obtener los progresos del usuario.");
-//     }
-
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     console.error("Error en GetExercisesByUser:", error);
-//     throw error;
-//   }
-// };
+// const GetProgressByUser = ({userId}) => {
+//   return records
+// }
 
 // export default GetProgressByUser;
+
+
+//   const records = [
+//     {
+//       id: 1,
+//       date: "2025-11-01",
+//       exercise: "Press de banca",
+//       reps: 12,
+//       sets: 4,
+//       weight: 65,
+//       effortLevel: 7,
+//     },
+//     {
+//       id: 2,
+//       date: "2025-10-28",
+//       exercise: "Sentadillas",
+//       reps: 10,
+//       sets: 3,
+//       weight: 80,
+//       effortLevel: 8,
+//     },
+//     {
+//       id: 3,
+//       date: "2025-10-25",
+//       exercise: "Trote",
+//       time: "25 min",
+//       distance: "4 km",
+//       rhythm: 90,
+//       effortLevel: 6,
+//     },
+//     {
+//       id: 4,
+//       date: "2025-10-20",
+//       exercise: "Peso muerto",
+//       reps: 8,
+//       sets: 3,
+//       weight: 90,
+//       effortLevel: 7,
+//     },
+//     {
+//       id: 5,
+//       date: "2025-10-15",
+//       exercise: "Press militar",
+//       reps: 10,
+//       sets: 3,
+//       weight: 40,
+//       effortLevel: 6,
+//     },
+//     {
+//       id: 6,
+//       date: "2025-10-10",
+//       exercise: "Trote",
+//       time: "20 min",
+//       distance: "3.5 km",
+//       effortLevel: 5,
+//     },
+//   ];
+
+
+import { baseurl } from "../../utils/constatn";
+
+const GetProgressByUser = async ({ userId }) => {
+  try {
+    const token = localStorage.getItem("accessToken");
+
+    if (!token) {
+      throw new Error("Token no encontrado. Por favor inicia sesión nuevamente.");
+    }
+
+    const response = await fetch(`${baseurl}/api/progress/user/${userId}`, {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json().catch(() => ({}));
+      throw new Error(errorData.message || "No se pudieron obtener los progresos del usuario.");
+    }
+
+    const data = await response.json();
+    console.log(data)
+    return data;
+  } catch (error) {
+    console.error("Error en GetExercisesByUser:", error);
+    throw error;
+  }
+};
+
+export default GetProgressByUser;
--- src/components/progress/ProgressRecord.jsx
+++ src/components/progress/ProgressRecord.jsx
@@ -1,17 +1,18 @@
 import { useState, useEffect } from "react";
 import Button from "../ui/Button";
 import GetProgressByUser from "../../api/progress/GetProgressByUser";
+import { useAuth } from "../../context/AuthContext";
 
 const ProgressRecord = () => {
   const [showAll, setShowAll] = useState(false);
-  const [progressList, setProgresList] = useState([])
-
+  const [progressList, setProgresList] = useState([]);
+  const { user } = useAuth();
 
   // try - catch
   useEffect(() => {
-    const handleProgressList = () => {
-      const userId = "1"
-      const data = GetProgressByUser({userId});
+    const handleProgressList = async () => {
+      // const userId = "1"
+      const data = await GetProgressByUser({ userId: user?.username });
       setProgresList(data);
     };
 
@@ -22,6 +23,7 @@ const ProgressRecord = () => {
 
   return (
     <div className="py-4 ">
+      {progressList.length === 0 && <div>No tiene progresos registrados</div>}
       <div className="grid gap-3">
         {visibleRecords.map((record) => (
           <div
@@ -29,8 +31,8 @@ const ProgressRecord = () => {
             className="bg-background p-4 rounded-md shadow-sm hover:shadow-md transition-all duration-150 hover:translate-y-1 hover:cursor-pointer"
           >
             <div className="flex justify-between items-center">
-              <p className="font-semibold text-lg">{record.exercise}</p>
-              <span className="text-sm text-gray-500">{record.date}</span>
+              <p className="font-semibold text-lg">{record.exerciseName}</p>
+              <span className="text-sm text-gray-500">{record.createdAt}</span>
             </div>
             <div className="mt-2 grid grid-cols-2 md:grid-cols-3 gap-y-1 text-sm text-gray-700 h-15">
               {record.weight && (
